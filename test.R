a279d4e679493b77f66d2e2c0385f0b6f550f660
Desktop 3070 <910886481@qq.com>
2021-04-18 22:47:59 +0800

    1

--- test.R
+++ test.R
@@ -1,20 +1,10 @@
-x=c(4.12,5.81,7.63,9.74,10.39,11.92,12.32,12.89,13.54,14.45)
-r=rank(abs(x-8))
-print(r)
-s=c()
-sgn=function(x){
-  if(x>0){
-    return(1)
-  }
-  else{
-    return(-1)
-  }
-}
-for (i in 1:length(x)) {
-  s=c(s,pnorm((1+r[i])/(length(x))+1))*sgn(x[i]-8)
-}
-print(s)
-t=sum(s)/sum(s^2)
-p=(1-pnorm(t))*2
-cat("t=",t,'\n',sep = '')
-cat('p-value=',p,'\n',sep = '')
+library(C50)
+data(churn)
+train<-churnTrain
+test<-churnTest
+str(train)
+train<-train[,-3]
+test<-test[,-3]
+train$churn<-factor(train$churn,levels = c("no","yes"),order=TRUE)
+test$churn <- factor(test$churn, ,levels = c('no','yes'), order = TRUE)
+model<-glm(churn~.,data=train,family = "binomial")
