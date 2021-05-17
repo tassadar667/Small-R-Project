b2d74f8e649e60b1eb290e52daaf8698c91ed570
Laptop 1060 <910886481@qq.com>
2021-05-17 18:44:28 +0800

    1

--- temp.R
+++ temp.R
@@ -1,2 +1,16 @@
-library(datarium)
-wilcox.test(mice$weight-25)
+g=function(x){
+  dt(x,5)
+}
+
+f=function(x){
+  g(x)/dnorm(x)
+}
+
+
+n=1000
+u=runif(n)
+norm_sample=rnorm(n)
+m=optimize(f, c(-10, 10), tol = 0.000001)$objective
+temp=m*dnorm(norm_sample)/g(norm_sample)
+target_sample=norm_sample[u<=temp]
+print(head(temp))
