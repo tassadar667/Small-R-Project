c893df38e50be7e02faa4dad77d7f153e7c36351
Laptop 1060 <910886481@qq.com>
2021-05-20 17:12:48 +0800

    1

--- temp.R
+++ temp.R
@@ -1,16 +1,38 @@
-g=function(x){
-  dt(x,5)
+library(foreign)
+library(quantreg)
+df=read.dta("./data/grilic.dta")
+x=cbind("s"=df$s,"iq"=df$iq,"expr"=df$expr,"tenure"=df$tenure,
+        "rns"=df$rns,"smas"=df$smsa)
+y=cbind("lw"=df$lw)
+osl=lm(y~x)
+summary(osl)
+
+lad=rq(y~x,tau=0.5)
+summary(lad)
+
+
+ans=c()
+for (i in seq(0.1,0.9,0.1)) {
+  temp=rq(y~x,tau=i)
+  ans=rbind(ans,temp$coefficients)
 }
+ans=cbind(ans,"x"=seq(0.1,0.9,0.1))
+library(ggplot2)
+library(data.table)
+ans=data.table(ans)
+ans=melt(ans,id="x")
+p=ggplot(ans,aes(x=x,y=value,group=variable,color=variable))+
+  geom_line()
+p
 
-f=function(x){
-  g(x)/dnorm(x)
+ans=c()
+for (i in seq(0.1,0.9,0.1)) {
+  temp=rq(y~x,tau=i)
+  ans=rbind(ans,temp$coefficients[-1])
 }
-
-
-n=1000
-u=runif(n)
-norm_sample=rnorm(n)
-m=optimize(f, c(-10, 10), tol = 0.000001)$objective
-temp=m*dnorm(norm_sample)/g(norm_sample)
-target_sample=norm_sample[u<=temp]
-print(head(temp))
+ans=cbind(ans,"x"=seq(0.1,0.9,0.1))
+ans=data.table(ans)
+ans=melt(ans,id="x")
+p=ggplot(ans,aes(x=x,y=value,group=variable,color=variable))+
+  geom_line()
+p
