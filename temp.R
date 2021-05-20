1ebd36371b30a969fdb59341cf565ba72ba79be5
Laptop 1060 <910886481@qq.com>
2021-05-20 21:54:04 +0800

    1

--- temp.R
+++ temp.R
@@ -1,7 +1,7 @@
 library(quantreg)
 df=read.table("./data/infant-birthweight.txt",header = TRUE)
 y=df$weight
-x=df[3:21]
-x=data.table(x)
+x=df[c(4,5,6,16)]
+x=as.matrix(x)
 lad=rq(y~x)
 summary(lad)
