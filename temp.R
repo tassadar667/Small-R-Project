60e9e8d7f5080af6b641c2e1b80bad5a5552f523
Laptop 1060 <910886481@qq.com>
2021-05-20 17:27:13 +0800

    1

--- temp.R
+++ temp.R
@@ -1,38 +1,7 @@
-library(foreign)
 library(quantreg)
-df=read.dta("./data/grilic.dta")
-x=cbind("s"=df$s,"iq"=df$iq,"expr"=df$expr,"tenure"=df$tenure,
-        "rns"=df$rns,"smas"=df$smsa)
-y=cbind("lw"=df$lw)
-osl=lm(y~x)
-summary(osl)
-
-lad=rq(y~x,tau=0.5)
+df=read.table("./data/infant-birthweight.txt",header = TRUE)
+y=df$weight
+x=df[3:21]
+x=data.table(x)
+lad=rq(y~x)
 summary(lad)
-
-
-ans=c()
-for (i in seq(0.1,0.9,0.1)) {
-  temp=rq(y~x,tau=i)
-  ans=rbind(ans,temp$coefficients)
-}
-ans=cbind(ans,"x"=seq(0.1,0.9,0.1))
-library(ggplot2)
-library(data.table)
-ans=data.table(ans)
-ans=melt(ans,id="x")
-p=ggplot(ans,aes(x=x,y=value,group=variable,color=variable))+
-  geom_line()
-p
-
-ans=c()
-for (i in seq(0.1,0.9,0.1)) {
-  temp=rq(y~x,tau=i)
-  ans=rbind(ans,temp$coefficients[-1])
-}
-ans=cbind(ans,"x"=seq(0.1,0.9,0.1))
-ans=data.table(ans)
-ans=melt(ans,id="x")
-p=ggplot(ans,aes(x=x,y=value,group=variable,color=variable))+
-  geom_line()
-p
